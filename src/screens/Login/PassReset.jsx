46aecdb7d0dc3271d733824db55a9f10eece41c0
Necro-Coder <nunezcotanoruben@gmail.com>
2024-11-08 17:19:32 +0100

    Initial Commit.

---
+++ src/screens/Login/PassReset.jsx
@@ -0,0 +1,7 @@
+export function PassReset() {
+  return (
+    <div>
+      <h1>PassReset</h1>
+    </div>
+  );
+}
---
+++ src/screens/Tips.jsx
@@ -0,0 +1,7 @@
+export function Tips() {
+  return (
+    <div>
+      <h1>Tips</h1>
+    </div>
+  );
+}
